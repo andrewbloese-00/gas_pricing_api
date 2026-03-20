77a06deb9691e2b30ed40045ceaba3ac2de69d17
andrewbloese-00 <andrewbloese@gmail.com>
2026-03-19 21:18:22 -0700

    initial commit / v0 of the API

---
+++ scraper.js
@@ -0,0 +1,94 @@
+import puppeteer from "puppeteer-extra"
+import StealthPlugin from "puppeteer-extra-plugin-stealth"
+
+const wait = (ms)=> new Promise(r=>setTimeout(r,ms))
+
+//configure stealth mode for puppeteer
+const stealth = StealthPlugin();
+puppeteer.use(stealth)
+
+
+//base url for gas buddy search results
+const BASE_GASBUDDY_URL = "https://www.gasbuddy.com/home"
+
+const USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
+
+export async function ScrapeGasBuddyAPI(zipcode,fuelGrade){
+    console.time("Scraper")
+    //launch headless browser
+    const browser = await puppeteer.launch({
+        headless: "shell"
+    });
+
+    try {
+        //open page & configure user agent to appear like a normal browser
+        const page = await browser.newPage()
+        await page.setUserAgent({userAgent:USER_AGENT})
+        await page.setExtraHTTPHeaders({
+            'accept-language': 'en-US,en;q=0.9',
+        });
+
+        //use args to construct request url 
+        const gasbuddy_url = `${BASE_GASBUDDY_URL}?search=${zipcode}&fuel=${fuelGrade}&method=all&maxAge=0`
+        await page.goto(gasbuddy_url,{
+            waitUntil: "networkidle2",
+            timeout: 10_000
+        })
+        const data = await page.evaluate(()=>{   
+            function getNodesWithClassLike(pattern,data=[],root=document.body){
+                if(typeof root.className == "string"){
+                    if(pattern instanceof RegExp){
+                        if(pattern.test(root.className)){
+                            data.push(root)
+                        } 
+                    } else if(typeof pattern == "string"){
+                        if(root.className.includes(pattern)) {
+                            data.push(root)
+                        }
+                    }
+                }
+                for(const childNode of root.children){  
+                    getNodesWithClassLike(pattern,data,childNode)
+                }
+            }
+            function getAveragePrice(){
+                let nodes = []
+                getNodesWithClassLike("StationDisplayPrice",nodes)
+                //get only spans that start with $
+                const prices = nodes
+                    .filter( n => n.tagName == "SPAN" && n.textContent[0] == "$")
+                    .map( span => Number(span.textContent.substring(1)))
+                    .filter( num => !isNaN(num))
+    
+    
+                const average = Number((
+                    prices.reduce((prev,current)=>prev+current,0) / Math.max(1,prices.length)
+                ).toFixed(2))
+    
+                const min = prices[0];
+                const max = prices.at(-1); 
+                return {min,max,average,prices}
+            }
+            return getAveragePrice()
+        })
+
+        console.log("Got Data")
+        return data 
+    } catch (error) {
+        console.error("[Scraper Failed]", error)
+        return { min : -1, max: -1, average: -1, prices: []}
+    } finally { 
+        if(browser){
+            await browser.close()
+        }
+        console.log("scraper finished and closed!")
+        console.timeEnd("Scraper")
+    }
+}
+
+
+// async function main(){
+//     await ScrapeGasBuddyAPI("85248")
+// }
+
+// main()
---
+++ server.js
@@ -0,0 +1,105 @@
+import http from "http"
+import { ScrapeGasBuddyAPI } from "./scraper.js";
+
+
+
+
+
+const PORT = 8080
+const CACHE_LIVE_MS = 1_800_000 //expire cached data every 30 mins
+const CACHE_CLEANUP_EVERY_N = 10
+/**@type {Map<string,PriceCacheItem>} */ const scraper_cache = new Map()
+
+let reqNum = 0
+
+//define server request handler
+const server = http.createServer(async (req,res)=>{
+    console.log(req.url)
+    const url = new URL(req.url,`http://localhost:${PORT}`);
+    //only GET requests allowed
+    if(req.method != "GET"){
+        res.writeHead(400)
+        return res.end(JSON.stringify({
+            error: "Invalid request method. only GET supported"
+        }))
+    }
+
+    //only route is "gas_price"
+    if(url.pathname != "/gas_price"){
+        res.writeHead(404)
+        return res.end(JSON.stringify({
+            error: `No route matches the path: "${url.pathname}"`
+        }))
+    }
+    
+    
+    //must provide a zip code
+    const zip = url.searchParams.get("zipcode")
+    if(!zip) {
+        res.writeHead(400)
+        return res.end(JSON.stringify({
+            error: "Expected query parameter 'zipcode' in request."
+        }))
+    } 
+
+    //valid request, increment counter
+    reqNum = (reqNum+1) % CACHE_CLEANUP_EVERY_N
+    
+    //params with defaults 
+    const fuelGrade = url.searchParams.get("fuel") ?? "1"    
+    
+    //check the cache for recent price data
+    const key = `${zip}:${fuelGrade}`
+    const entry = scraper_cache.get(key)
+    const now = Date.now()
+    if(entry && entry.expires_ms > now){
+        res.writeHead(200)
+        return res.end(JSON.stringify(entry.price_data))
+    }
+
+    //use the scraper to get price data 
+    const data = await ScrapeGasBuddyAPI(zip,fuelGrade);
+
+    //clean up the cache every 'n' request
+    if(reqNum == CACHE_CLEANUP_EVERY_N-1){
+        for(const key of scraper_cache.keys() ){
+            const value = scraper_cache.get(key)
+            //remove expired entries
+            if(value.expires_ms < now){
+                scraper_cache.delete(key)
+            }
+        }
+    }
+    if(!data || data.prices.length == 0) {
+        res.writeHead(400)
+        return res.end(JSON.stringify({
+            error: "Failed to fetch gas price data. Please try again later."
+        }))
+    }
+    
+    scraper_cache.set( key ,{ expires_ms: now + CACHE_LIVE_MS , price_data: data})
+    res.writeHead(200)
+    return res.end(JSON.stringify(data,null,4));
+})
+
+server.listen(PORT,()=>{
+    console.log(`Gas API listening on :${PORT}`)
+})
+
+/**
+ * @typedef {Object} PriceData
+ * @property {number} min
+ * @property {number} max
+ * @property {number} average
+ * @property {number[]} prices
+ */
+
+/**
+* @typedef {Object} PriceCacheItem
+* @property {PriceData} price_data
+* @property {number} expires_ms 
+*/
+
+
+
+
